9aad0cce9db3aad825f02167612fa6690209e3ac
carlosmilenaquesada_portatil <carlosmilenaquesada@gmail.com>
2023-10-10 17:14:20 +0200

    ultimo

--- app/src/main/java/com/example/promul_chat_con_dos_activities/MainActivity.java
+++ app/src/main/java/com/example/promul_chat_con_dos_activities/MainActivity.java
@@ -2,13 +2,79 @@ package com.example.promul_chat_con_dos_activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tituloMain;
+    TextView textoChatMain;
+    EditText mensajeAEnviarPorMain;
+    Button bontonEnviarMain;
+
+    public String MENSAJE_DE_MAIN = "com.example.promul_chat_con_dos_activities.MainActivity";
+
+    public int PETICION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tituloMain = (TextView) findViewById(R.id.tituloMain);
+        textoChatMain = (TextView) findViewById(R.id.textoChatMain);
+        mensajeAEnviarPorMain = (EditText) findViewById(R.id.mensajeAEnviarPorMain);
+        bontonEnviarMain = (Button) findViewById(R.id.botonEnviarMain);
+
     }
+
+
+    public void enviarPorMain(View view){
+        String mensajeMain = String.valueOf(mensajeAEnviarPorMain.getText());
+        Intent intent = new Intent();
+        intent.putExtra(MENSAJE_DE_MAIN, mensajeMain);
+        startActivity(intent);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/promul_chat_con_dos_activities/SecondActivity.java
+++ app/src/main/java/com/example/promul_chat_con_dos_activities/SecondActivity.java
@@ -2,13 +2,43 @@ package com.example.promul_chat_con_dos_activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView tituloSecond;
+    TextView textoChatSecond;
+    EditText mensajeAEnviarPorSecond;
+    Button botonEnviarSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        /*Intent intent = getIntent();
+
+        tituloSecond = (TextView) findViewById(R.id.tituloSecond);
+        textoChatSecond = (TextView) findViewById(R.id.textoChatSecond);
+        mensajeAEnviarPorSecond = (EditText) findViewById(R.id.mensajeAEnviarPorSecond);
+        botonEnviarSecond = (Button) findViewById(R.id.botonEnviarSecond);*/
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
